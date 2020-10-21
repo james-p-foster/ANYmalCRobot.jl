f84e97921888b7d594a2ec4e9da7a5ca36be6604
James Foster <james.paul.foster@outlook.com>
2020-10-21 11:32:31 +0100

    Adding dependencies and functionality

---
+++ examples/example.jl
@@ -0,0 +1,11 @@
+using ANYmalCRobot
+using MeshCat
+using MeshCatMechanisms
+using RigidBodyDynamics
+
+anymal = ANYmalCRobot.mechanism()
+vis = Visualizer()
+mvis = MechanismVisualizer(
+    anymal,
+    URDFVisuals(ANYmalCRobot.urdfpath(), package_path=[ANYmalCRobot.package_path()]),
+    vis)
--- src/ANYmalCRobot.jl
+++ src/ANYmalCRobot.jl
@@ -1,5 +1,39 @@
 module ANYmalCRobot
 
-# Write your package code here.
+using RigidBodyDynamics
+using RigidBodyDynamics.Contact
 
+packagepath() = joinpath(@__DIR__, "..")
+urdfpath() = joinpath(packagepath(), "anymal_c_simple_description/urdf/anymal.urdf")
+
+function default_contact_model()
+    SoftContactModel(hunt_crossley_hertz(k = 500e3), ViscoelasticCoulombModel(0.8, 20e3, 100.))
 end
+
+function mechanism(::Type{T} = Float64;
+    floating = true,
+    contactmodel = default_contact_model(),
+    remove_fixed_tree_joints = false, add_flat_ground = false) where {T}
+    mechanism = RigidBodyDynamics.parse_urdf(urdfpath(); 
+        scalar_type = T, 
+        floating = floating, 
+        remove_fixed_tree_joints = remove_fixed_tree_joints)
+
+    if add_flat_ground
+        frame = root_frame(mechanism)
+        ground = HalfSpace3D(Point3D(frame, 0., 0., 0.), FreeVector3D(frame, 0., 0., 1.))
+        add_environment_primitive!(mechanism, ground)
+    end
+
+    remove_fixed_tree_joints && remove_fixed_tree_joints!(mechanism)
+
+    return mechanism
+end
+
+function __init__()
+    if !isfile(urdfpath())
+        error("Could not find $(urdfpath()). Please run `import Pkg; Pkg.build(\"MiniCheetahRobot\")`.")
+    end
+end
+
+end # module
