ad6b0738e9389d5bcae3f10bdeba60e0038ff058
James Foster <james.paul.foster@outlook.com>
2020-10-21 11:49:00 +0100

    Fixed typo package_path() --> packagepath()

--- examples/example.jl
+++ examples/example.jl
@@ -7,5 +7,5 @@ anymal = ANYmalCRobot.mechanism()
 vis = Visualizer()
 mvis = MechanismVisualizer(
     anymal,
-    URDFVisuals(ANYmalCRobot.urdfpath(), package_path=[ANYmalCRobot.package_path()]),
+    URDFVisuals(ANYmalCRobot.urdfpath(), package_path=[ANYmalCRobot.packagepath()]),
     vis)
